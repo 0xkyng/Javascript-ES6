5d5544f7fabe5a505cf643a06c32d587c3a112e6
Isaac Wanger <teryima127@gmail.com>
2023-03-24 14:03:05 +0100

    modal window

--- index.js
+++ index.js
@@ -143,4 +143,10 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  };
+  
+
 
