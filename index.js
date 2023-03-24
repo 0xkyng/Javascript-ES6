22b32a1cfe588e09a1c6a9daa553454fbd2a272b
Isaac Wanger <teryima127@gmail.com>
2023-03-24 11:48:41 +0100

    guess my number

--- index.js
+++ index.js
@@ -60,3 +60,13 @@ let highscore = 0;
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+  
+    // When there is no input
+    if (!guess) {
+      // document.querySelector('.message').textContent = '⛔️ No number!';
+      displayMessage('⛔️ No number!');
+  
