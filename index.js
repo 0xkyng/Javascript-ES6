ad8d2562d3f235bac551ea0568aff1801350f9f1
Isaac Wanger <teryima127@gmail.com>
2023-03-27 11:14:56 +0100

    functions

--- index.js
+++ index.js
@@ -474,32 +474,32 @@ console.log(typeof new String('jonas').slice(1));
 ///////////////////////////////////////
 
 // Default Parameters
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  // ES5
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 2);
-createBooking('LH123', 5);
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
 
-createBooking('LH123', undefined, 1000);
+// createBooking('LH123', undefined, 1000);
 
 
 ///////////////////////////////////////
